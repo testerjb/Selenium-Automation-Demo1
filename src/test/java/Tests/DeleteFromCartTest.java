da075013dfd3465ce567b9437e3da74460784163
Jill¨ <urbanjill1@gmail.com>
2025-02-26 22:04:03 -0600

    fixing error chromedriver

--- src/test/java/Tests/DeleteFromCartTest.java
+++ src/test/java/Tests/DeleteFromCartTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -22,8 +23,11 @@ public class DeleteFromCartTest {
 
     @BeforeMethod
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
-        driver=new ChromeDriver();
+    	System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+    	ChromeOptions opt = new ChromeOptions();
+    	opt.addArguments("--remote-allow-origins=*");
+    	// Launching the browser    	   	    	        
+        driver=new ChromeDriver(opt);     
         driver.get(pageLink);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
